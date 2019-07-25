44a6843003d3a3b5738862b9afec883a9e9d0ad1
wagner <wagner.coder@gmail.com>
2019-07-25 03:53:04 -0300

    List extension and create folders by extension name

--- list_file.py
+++ list_file.py
@@ -1,13 +1,20 @@
 import os
 import glob
+import re
 
 #get base dir name
 dirpath = os.getcwd()
 
 folders = []
+extensions = []
 
 #list files from dir
 files = [f for f in glob.glob(dirpath + "**/*", recursive = True)]
 
-for f in files:
-    print(f)
+#list file extensions
+for f in files: 
+    aux = f.split('.', 1)
+    
+    #this means it is a file not a folder, and if the folder do not exists
+    if(len(aux) > 1 and not os.path.exists(aux[1])): 
+        os.mkdir(aux[1])
